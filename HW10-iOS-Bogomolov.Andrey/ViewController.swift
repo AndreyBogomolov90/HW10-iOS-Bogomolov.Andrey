77a660d50990e8e36c2a39af108d8590364eb22c
Andrey Bogomolov <aab90@list.ru>
2022-08-05 06:18:07 +0300

    remove-comments
    Remove all comments in project.

--- HW10-iOS-Bogomolov.Andrey/ViewController.swift
+++ HW10-iOS-Bogomolov.Andrey/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  HW10-iOS-Bogomolov.Andrey
-//
-//  Created by Andrey Bogomolov on 03.08.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
-
